feb5ec30189ccd16668025508aeb7253e077d0c7
dev00 <michael.rose@t-systems.com>
2019-08-09 11:07:34 +0000

    fix indent

--- os_example/os_buildenv.tf
+++ os_example/os_buildenv.tf
@@ -1,124 +1,124 @@
 variable "auth_url" {
-	type		= string
-	default	=	"https://iam.eu-de.otc.t-systems.com:443/v3"
+  type    = string
+  default = "https://iam.eu-de.otc.t-systems.com:443/v3"
 }
 variable "project" {
-  type		= string
-	default	=	"TFTEST"
+  type    = string
+  default = "TFTEST"
 }
 
 variable "vpc_cidr" {
-  default	= "192.168.0.0/16"
+  default = "192.168.0.0/16"
 }
 variable "subnet_1_cidr" {
-  default	= "192.168.10.0/24"
+  default = "192.168.10.0/24"
 }
 variable "subnet_1_gw" {
-  default	= "192.168.10.1"
+  default = "192.168.10.1"
 }
 
 variable "dns_servers" {
-	type		= list
-	default	= ["9.9.9.9", "8.8.8.8"]
+  type    = list
+  default = ["9.9.9.9", "8.8.8.8"]
 }
 
 variable "external_network_id" {
-	default = "0a2228f2-7f8a-45f1-8e09-9039e1d09975"
+  default = "0a2228f2-7f8a-45f1-8e09-9039e1d09975"
 }
 
 variable "public_key" {}
 
 variable "instances" {
-	type		= list
-	default = [{
-			"name"   = "ubuntu1604"
-			"image"	 = "Standard_Ubuntu_16.04_latest"
-		},
-		{
-			"name"   = "ubuntu1804"
-			"image"	 = "Standard_Ubuntu_18.04_latest"
-		},
-		{
-			"name"   = "ubuntu1604-tsi"
-			"image"	 = "Community_Ubuntu_16.04_TSI_latest"
-		},
-		{
-			"name"   = "centos7"
-			"image"	 = "Standard_CentOS_7_latest"
-		},
-		{
-			"name"   = "rhel7"
-			"image"	 = "Enterprise_RedHat_7_latest"
-		}
-	]
+  type    = list
+  default = [{
+      "name"   = "ubuntu1604"
+      "image"  = "Standard_Ubuntu_16.04_latest"
+    },
+    {
+      "name"   = "ubuntu1804"
+      "image"  = "Standard_Ubuntu_18.04_latest"
+    },
+    {
+      "name"   = "ubuntu1604-tsi"
+      "image"  = "Community_Ubuntu_16.04_TSI_latest"
+    },
+    {
+      "name"   = "centos7"
+      "image"  = "Standard_CentOS_7_latest"
+    },
+    {
+      "name"   = "rhel7"
+      "image"  = "Enterprise_RedHat_7_latest"
+    }
+  ]
 }
 
 provider "openstack" {
 }
 
 resource "openstack_networking_secgroup_v2" "allow_ssh_grp" {
-  name		= "${var.project}-allow_ssh"
-  description	= "allowing ssh from everywhere"
+  name    = "${var.project}-allow_ssh"
+  description = "allowing ssh from everywhere"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "allow_ssh_rule" {
-	direction			= "ingress"
-	ethertype			= "IPv4"
-	protocol			= "tcp"
-	port_range_min	=	22
-	port_range_max	=	22
-	remote_ip_prefix = "0.0.0.0/0"
-	security_group_id = "${openstack_networking_secgroup_v2.allow_ssh_grp.id}"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.allow_ssh_grp.id}"
 }
 
 resource "openstack_networking_network_v2" "os_network_1" {
-	name					= "${var.project}-network"
-	admin_state_up	=	true
+  name              = "${var.project}-network"
+  admin_state_up    = true
 }
 
 resource "openstack_networking_subnet_v2" "os_subnet_1" {
-	name					= "${var.project}-subnet1"
-	cidr					=	"${var.subnet_1_cidr}"
-	dns_nameservers	= "${var.dns_servers}"
-	gateway_ip		= "${var.subnet_1_gw}"
-	network_id		= "${openstack_networking_network_v2.os_network_1.id}"
+  name              = "${var.project}-subnet1"
+  cidr              = "${var.subnet_1_cidr}"
+  dns_nameservers   = "${var.dns_servers}"
+  gateway_ip        = "${var.subnet_1_gw}"
+  network_id        = "${openstack_networking_network_v2.os_network_1.id}"
 }
 
 resource "openstack_networking_router_v2" "os_router" {
-	# calling the router vpc, because that's the name that will show up
-	# in the Web Interface. Calling it router may confuse people
-	name					= "${var.project}-vpc"
-	admin_state_up	= true
-	external_network_id	= "${var.external_network_id}"
-	enable_snat		= true
+  # calling the router vpc, because that's the name that will show up
+  # in the Web Interface. Calling it router may confuse people
+  name             = "${var.project}-vpc"
+  admin_state_up   = true
+  external_network_id = "${var.external_network_id}"
+  enable_snat      = true
 }
 
 resource "openstack_networking_router_interface_v2" "os_router_if" {
-	router_id			=	"${openstack_networking_router_v2.os_router.id}"
-	subnet_id			= "${openstack_networking_subnet_v2.os_subnet_1.id}"
+  router_id        = "${openstack_networking_router_v2.os_router.id}"
+  subnet_id        = "${openstack_networking_subnet_v2.os_subnet_1.id}"
 }
 
 resource "openstack_compute_keypair_v2" "os_keypair" {
-	name					= "${var.project}_pubkey"
-  public_key		= "${var.public_key}"
+  name             = "${var.project}_pubkey"
+  public_key       = "${var.public_key}"
 }
 
 resource "openstack_compute_instance_v2" "instances" {
-	# can later be replaced by a for_each, which is currenctly not implemented
-	# for a resource - see 'Resource for_each' at
-	# https://www.hashicorp.com/blog/hashicorp-terraform-0-12-preview-for-and-for-each
-	count						= length(var.instances)
+  # can later be replaced by a for_each, which is currenctly not implemented
+  # for a resource - see 'Resource for_each' at
+  # https://www.hashicorp.com/blog/hashicorp-terraform-0-12-preview-for-and-for-each
+  count           = length(var.instances)
 
-  name						= "${var.project}-${var.instances[count.index].name}"
-  image_name			= "${var.instances[count.index].image}"
-  flavor_name			= "s2.medium.2"
+  name            = "${var.project}-${var.instances[count.index].name}"
+  image_name      = "${var.instances[count.index].image}"
+  flavor_name     = "s2.medium.2"
   security_groups = ["default","${openstack_networking_secgroup_v2.allow_ssh_grp.id}"]
-  key_pair				= "${openstack_compute_keypair_v2.os_keypair.id}"
+  key_pair        = "${openstack_compute_keypair_v2.os_keypair.id}"
   network {
     uuid = "${openstack_networking_network_v2.os_network_1.id}"
-	}
-	metadata				= {
-		env						= "testing"
-		foo						= "bar"
-	}
+  }
+  metadata        = {
+    env           = "testing"
+    foo           = "bar"
+  }
 }
--- otc_example/otc_buildenv.tf
+++ otc_example/otc_buildenv.tf
@@ -1,105 +1,105 @@
 variable "auth_url" {
-	type		= string
-	default	=	"https://iam.eu-de.otc.t-systems.com:443/v3"
+  type    = string
+  default = "https://iam.eu-de.otc.t-systems.com:443/v3"
 }
 variable "project" {
-  type		= string
-	default	=	"TFTEST"
+  type    = string
+  default = "TFTEST"
 }
 
 variable "vpc_cidr" {
-  default	= "192.168.0.0/16"
+  default = "192.168.0.0/16"
 }
 variable "subnet_1_cidr" {
-  default	= "192.168.10.0/24"
+  default = "192.168.10.0/24"
 }
 variable "subnet_1_gw" {
-  default	= "192.168.10.1"
+  default = "192.168.10.1"
 }
 
 variable "dns_servers" {
-	type		= list
-	default	= ["9.9.9.9", "8.8.8.8"]
+  type    = list
+  default = ["9.9.9.9", "8.8.8.8"]
 }
 
 variable "public_key" {}
 
 variable "instances" {
-	type		= list
-	default = [{
-			"name"   = "ubuntu1604"
-			"image"	 = "Standard_Ubuntu_16.04_latest"
-		},
-		{
-			"name"   = "ubuntu1804"
-			"image"	 = "Standard_Ubuntu_18.04_latest"
-		},
-		{
-			"name"   = "ubuntu1604-tsi"
-			"image"	 = "Community_Ubuntu_16.04_TSI_latest"
-		},
-		{
-			"name"   = "centos7"
-			"image"	 = "Standard_CentOS_7_latest"
-		},
-		{
-			"name"   = "rhel7"
-			"image"	 = "Enterprise_RedHat_7_latest"
-		}
-	]
+  type    = list
+  default = [{
+      "name"   = "ubuntu1604"
+      "image"  = "Standard_Ubuntu_16.04_latest"
+    },
+    {
+      "name"   = "ubuntu1804"
+      "image"  = "Standard_Ubuntu_18.04_latest"
+    },
+    {
+      "name"   = "ubuntu1604-tsi"
+      "image"  = "Community_Ubuntu_16.04_TSI_latest"
+    },
+    {
+      "name"   = "centos7"
+      "image"  = "Standard_CentOS_7_latest"
+    },
+    {
+      "name"   = "rhel7"
+      "image"  = "Enterprise_RedHat_7_latest"
+    }
+  ]
 }
 
 provider "opentelekomcloud" {
 }
 
 resource "opentelekomcloud_compute_secgroup_v2" "otc_allow_ssh" {
-  name		= "${var.project}-allow_ssh"
-  description	= "allowing ssh from everywhere"
+  name    = "${var.project}-allow_ssh"
+  description = "allowing ssh from everywhere"
 
   rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
+    from_port     = 22
+    to_port       = 22
+    ip_protocol   = "tcp"
+    cidr          = "0.0.0.0/0"
   }
 }
 
 resource "opentelekomcloud_vpc_v1" "otc_vpc" {
-  name		= "${var.project}-vpc"
-  cidr		= "${var.vpc_cidr}"
-	shared	= true
+  name            = "${var.project}-vpc"
+  cidr            = "${var.vpc_cidr}"
+  shared          = true
 
 }
 resource "opentelekomcloud_vpc_subnet_v1" "otc_subnet_1" {
-  name						= "${var.project}-subnet1"
-  cidr						= "${var.subnet_1_cidr}"
-  gateway_ip			= "${var.subnet_1_gw}"
-  vpc_id					= "${opentelekomcloud_vpc_v1.otc_vpc.id}"
-	primary_dns			=	"${var.dns_servers[0]}"
-	secondary_dns		= "${var.dns_servers[1]}"
+  name            = "${var.project}-subnet1"
+  cidr            = "${var.subnet_1_cidr}"
+  gateway_ip      = "${var.subnet_1_gw}"
+  vpc_id          = "${opentelekomcloud_vpc_v1.otc_vpc.id}"
+  primary_dns     = "${var.dns_servers[0]}"
+  secondary_dns   = "${var.dns_servers[1]}"
 }
 
 resource "opentelekomcloud_compute_keypair_v2" "otc_keypair" {
-	name					= "${var.project}_pubkey"
-  public_key		= "${var.public_key}"
+  name            = "${var.project}_pubkey"
+  public_key      = "${var.public_key}"
 }
 
 resource "opentelekomcloud_compute_instance_v2" "instances" {
-	# can later be replaced by a for_each, which is currenctly not implemented
-	# for a resource - see 'Resource for_each' at
-	# https://www.hashicorp.com/blog/hashicorp-terraform-0-12-preview-for-and-for-each
-	count						= length(var.instances)
+  # can later be replaced by a for_each, which is currenctly not implemented
+  # for a resource - see 'Resource for_each' at
+  # https://www.hashicorp.com/blog/hashicorp-terraform-0-12-preview-for-and-for-each
+  count           = length(var.instances)
 
-  name						= "${var.project}-${var.instances[count.index].name}"
-  image_name			= "${var.instances[count.index].image}"
-  flavor_name			= "s2.medium.2"
+  name            = "${var.project}-${var.instances[count.index].name}"
+  image_name      = "${var.instances[count.index].image}"
+  flavor_name     = "s2.medium.2"
   security_groups = ["default","${opentelekomcloud_compute_secgroup_v2.otc_allow_ssh.id}"]
-  key_pair				= "${opentelekomcloud_compute_keypair_v2.otc_keypair.id}"
+  key_pair        = "${opentelekomcloud_compute_keypair_v2.otc_keypair.id}"
   network {
-    uuid = "${opentelekomcloud_vpc_subnet_v1.otc_subnet_1.id}"
+    uuid          = "${opentelekomcloud_vpc_subnet_v1.otc_subnet_1.id}"
   }
-	tag		 = {
-		env					= "testing"
-		foo					= "bar"
-	}
+  tag             = {
+    env           = "testing"
+    foo           = "bar"
+  }
 }
